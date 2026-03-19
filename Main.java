edf4e579ea69b4be4859926565a0938c4d9ff8dd
ashmitsharma-09 <ashmitshrma0999@gmail.com>
2026-03-19 15:14:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,88 +1,38 @@
-package com.gla.interfaces.problem6;
+package com.gla.interfaces.problem3;
 
-// 1. Payment Gateway Integration
-interface PaymentProcessor {
-    void processPayment(double amount);
-
-    default void refund(double amount) {
-        System.out.println("Refund of " + amount + " processed.");
-    }
+interface DigitalPayment {
+    void pay(double amount);
 }
 
-class PayPal implements PaymentProcessor {
+class UPI implements DigitalPayment {
     @Override
-    public void processPayment(double amount) {
-        System.out.println("PayPal payment: " + amount);
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using UPI");
     }
 }
 
-// 2. Data Export Feature
-interface DataExporter {
-    void export();
-
-    default void exportToJSON() {
-        System.out.println("Exporting to JSON...");
-    }
-}
-
-class Report implements DataExporter {
+class CreditCard implements DigitalPayment {
     @Override
-    public void export() {
-        System.out.println("Exporting data...");
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card");
     }
 }
 
-// 3. Smart Vehicle Dashboard
-interface Vehicle {
-    void displaySpeed();
-
-    default void displayBattery() {
-        System.out.println("Battery display not supported for this vehicle.");
-    }
-}
-
-class ElectricCar implements Vehicle {
+class Wallet implements DigitalPayment {
     @Override
-    public void displaySpeed() {
-        System.out.println("Speed: 100 km/h");
-    }
-
-    @Override
-    public void displayBattery() {
-        System.out.println("Battery: 85%");
-    }
-}
-
-class PetrolCar implements Vehicle {
-    @Override
-    public void displaySpeed() {
-        System.out.println("Speed: 120 km/h");
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Wallet");
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // 1. Payment
-        PaymentProcessor pp = new PayPal();
-        pp.processPayment(100);
-        pp.refund(50);
+        DigitalPayment upi = new UPI();
+        DigitalPayment card = new CreditCard();
+        DigitalPayment wallet = new Wallet();
 
-        System.out.println("---");
-
-        // 2. Data Export
-        DataExporter de = new Report();
-        de.export();
-        de.exportToJSON();
-
-        System.out.println("---");
-
-        // 3. Vehicle
-        Vehicle ev = new ElectricCar();
-        ev.displaySpeed();
-        ev.displayBattery();
-
-        Vehicle pv = new PetrolCar();
-        pv.displaySpeed();
-        pv.displayBattery();
+        upi.pay(500);
+        card.pay(1000);
+        wallet.pay(250);
     }
 }
